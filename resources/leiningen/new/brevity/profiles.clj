726866f12bfafc2ba82d9f56be7f7268e1cd7d24
Joshua Maddux <josh@pkcsecurity.com>
2018-07-11 11:17:39 -0700

    Use environ for environment variables

---
+++ resources/leiningen/new/brevity/profiles.clj
@@ -0,0 +1,8 @@
+{:profiles/dev {:env {:environment "development"
+                      :host "127.0.0.1"
+                      :port "8080"
+                      :auth-secret "TODO"
+                      :auth-issuer "TODO"
+                      :auth-audience "TODO"
+                      :auth-token-name "TODO"
+                      :sql-server ""}}}
--- resources/leiningen/new/brevity/src/brevity/clj/startup/core.clj
+++
@@ -1,6 +0,0 @@
-(ns {{name}}.clj.startup.core
-  (:require [{{name}}.clj.startup.properties :as p]))
-
-(defn startup []
-  "Runs before server start time to prepare the application for runtime."
-  (p/push-configs))
