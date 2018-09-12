8d7517cb3e24f9d3b722b43390fe6345c58b76d2
Joshua Maddux <josh@pkcsecurity.com>
2018-09-12 12:19:56 -0700

    Add connection pooling with hikari

--- resources/leiningen/new/brevity/src/brevity/clj/models/sql.clj
+++ resources/leiningen/new/brevity/src/brevity/clj/models/sql.clj
@@ -3,18 +3,23 @@
             [environ.core :as environ]
             [hugsql.core :as hug]
             [hugsql-adapter-case.adapters :as adapters]
+            [hikari-cp.core :as hikari]
             [conman.core :as conman])
   (:import [com.opentable.db.postgres.embedded EmbeddedPostgres]
            [java.io File]))
 
-; Brevity doesn't yet do any connection pooling by default.  If you have a need for connection pooling, see:
-; https://github.com/luminus-framework/conman#using-bind-connection
-(def dbspec (environ/env :database-url))
+(def connection-uri (environ/env :jdbc-database-url))
+
+(def dbspec
+  (delay {:datasource (hikari/make-datasource
+                        {:jdbc-url connection-uri
+                         :maximum-pool-size (or (environ/env :hikari-maximum-pool-size) 10)
+                         :minimum-idle (or (environ/env :hikari-minimum-idle) 10)})}))
 
 (hug/set-adapter! (adapters/kebab-adapter))
 
-(conman/bind-connection dbspec "{{raw-name}}/sql/articles.sql")
-(conman/bind-connection dbspec "{{raw-name}}/sql/users.sql")
+(conman/bind-connection @dbspec "brevitytestproject/sql/articles.sql")
+(conman/bind-connection @dbspec "brevitytestproject/sql/users.sql")
 
 (defn init! []
       (let [dev-mode? (= "true" (environ/env :dev-database))]
